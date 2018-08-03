2717cb1b17e6f50d8babd05418c5d238975d8a90
markalos <goknowy@gmail.com>
2018-08-03 14:39:01 +0800

    keyboard

--- widget/LabelText.py
+++ widget/LabelText.py
@@ -1,11 +1,21 @@
 from PyQt4 import QtGui, QtCore
 
+class MatchBoxLineEdit(QtGui.QLineEdit):
+	def focusInEvent(self, e):
+		try:
+			subprocess.Popen(["matchbox-keyboard"])
+		except FileNotFoundError:
+			pass
+
+	def focusOutEvent(self,e):
+		subprocess.Popen(["killall","matchbox-keyboard"])
+
 class LabelText(QtGui.QWidget):
 	"""edit text box with label"""
 	def __init__(self, label):
 		QtGui.QWidget.__init__(self)
 		
-		self.editText = QtGui.QLineEdit()
+		self.editText = MatchBoxLineEdit()
 		layout = QtGui.QHBoxLayout()
 		layout.addWidget(QtGui.QLabel(label))
 
