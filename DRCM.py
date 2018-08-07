f25a3941c5ddda777236c1a544f5440775871b0c
markalos <goknowy@gmail.com>
2018-08-07 20:53:03 +0800

    exposure time

--- DRCM.py
+++ DRCM.py
@@ -69,7 +69,7 @@ class VideoReader():
 		try:
 			self.reader.set(3,640);
 			self.reader.set(4,480);
-			# self.reader.set(38, 1)
+			self.reader.set(38, 4)
 		except Exception as e:
 			print (str(e))
 			logger.exception(e)
